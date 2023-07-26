d58ff829b90fa3444dfdd766cba89080d8856cac
Emmanuel Koomson <devfii@gmail.com>
2023-07-26 09:50:57 +0000

    added variables for ami and instance_type

--- iac/variables.tf
+++ iac/variables.tf
@@ -3,3 +3,15 @@ variable "region" {
     type = string
 
 }
+
+variable "ami" {
+    description = "AMI for EC2 instance"
+    type = string
+
+}
+
+variable "instance_type" {
+    description = "EC2 Instance type"
+    type = string
+
+}

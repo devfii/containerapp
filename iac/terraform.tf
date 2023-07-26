29fd3587cad6454ec62890570c62928f105dc7a5
Emmanuel Koomson <devfii@gmail.com>
2023-07-26 08:03:37 +0000

    added random provider

--- iac/terraform.tf
+++ iac/terraform.tf
@@ -12,6 +12,10 @@ terraform {
         source = "hashicorp/aws"
         version = "~> 5.9.0"
     }
+    random = {
+      source = "hashicorp/random"
+      version = "~> 3.5.1"
+    }
   }
 
   required_version = "~> 1.5.0"
